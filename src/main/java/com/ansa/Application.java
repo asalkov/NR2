785d791a05e4e6f86bef0795e56217a0bfa185f0
Andrey Salkov <salkov.andrey@gmail.com>
2015-11-30 00:54:04 +0300

    fix

--- src/main/java/com/ansa/Application.java
+++ src/main/java/com/ansa/Application.java
@@ -2,7 +2,6 @@ package com.ansa;
 
 import com.ansa.usermanagement.USER_ROLE;
 import com.ansa.usermanagement.User;
-import com.ansa.usermanagement.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -14,20 +13,9 @@ import javax.annotation.PostConstruct;
 @EnableAutoConfiguration
 public class Application {
 
-    @Autowired
-    UserRepository userRepository;
-
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @PostConstruct
-    public void init(){
-        User user = new User("username", "password", USER_ROLE.GENERAL);
-        userRepository.save(user);
-        Iterable<User> userList = userRepository.findAll();
 
-         userList.forEach(user1 -> System.out.println(user1.getId()));
-    }
 }

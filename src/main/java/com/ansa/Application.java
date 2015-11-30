cc806920386091996be4b09edfa457fb16345de4
unknown <asalkov@ASALKOV.luxoft.com>
2015-11-30 19:49:07 +0300

    views

--- src/main/java/com/ansa/Application.java
+++ src/main/java/com/ansa/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+/*
+http://jasonwatmore.com/post/2015/03/10/AngularJS-User-Registration-and-Login-Example.aspx
+ */
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,20 +1,51 @@
-var xAuthTokenHeaderName = 'x-auth-token';
+(function () {
+    'use strict';
 
-var newsRoomApp = angular.module('newsRoomApp', ['ngRoute', 'ngCookies', 'newsRoomApp.services']);
+    angular
+        .module('app', ['ngRoute', 'ngCookies'])
+        .config(config)
+        .run(run);
 
-newsRoomApp.config()
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                templateUrl: 'home/home.view.html',
+                controllerAs: 'vm'
+            })
 
-var services = angular.module('newsRoomApp.services', ['ngResource']);
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'login/login.view.html',
+                controllerAs: 'vm'
+            })
 
-services.factory('LoginService', function($resource) {
+            .when('/register', {
+                controller: 'RegisterController',
+                templateUrl: 'register/register.view.html',
+                controllerAs: 'vm'
+            })
 
-    return $resource(':action', {},
-        {
-            authenticate: {
-                method: 'POST',
-                params: {'action' : 'authenticate'},
-                headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-            }
+            .otherwise({ redirectTo: '/login' });
+    }
+
+    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+    function run($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
         }
-    );
-});
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/login');
+            }
+        });
+    }
+
+})();
